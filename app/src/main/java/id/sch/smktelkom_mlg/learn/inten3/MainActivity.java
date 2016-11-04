3e0843ef5fabd1f4ccfc53250514bf8ace202552
primalistianti10 <primalistianti10@gmail.com>
2016-11-04 13:31:58 +0700

    Intent Camera (Photo)

--- app/src/main/java/id/sch/smktelkom_mlg/learn/inten3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/inten3/MainActivity.java
@@ -1,15 +1,25 @@
 package id.sch.smktelkom_mlg.learn.inten3;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        findViewById(R.id.imageViewCamera)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        capturePhoto();
+                    }
+                });
         findViewById(R.id.imageViewBrowser)
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -33,6 +43,24 @@ public class MainActivity extends AppCompatActivity {
                 });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void capturePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) !=null)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode== REQUEST_IMAGE_CAPTURE && resultCode==RESULT_OK)
+        {
+            Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView iv = (ImageView) findViewById(R.id.imageViewCamera);
+            iv.setImageBitmap(bitmap);
+        }
     }
 
     private void openWebPage(String url) {
